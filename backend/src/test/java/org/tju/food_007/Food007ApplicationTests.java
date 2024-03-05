36cda43686544786afe08ec17ffc7a02d59f8420
wst2003 <3304405246@qq.com>
2024-03-05 10:55:47 +0800

    add ObsTool

--- backend/src/test/java/org/tju/food_007/Food007ApplicationTests.java
+++ backend/src/test/java/org/tju/food_007/Food007ApplicationTests.java
@@ -2,12 +2,14 @@ package org.tju.food_007;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.tju.food_007.utils.ObsOperationTool;
 
 @SpringBootTest
 class Food007ApplicationTests {
 
     @Test
     void contextLoads() {
+        ObsOperationTool.uploadFile("","1.png","C:\\Users\\33044\\Pictures\\3.4报销\\转接头30.6.jpg");
     }
 
 }
