153c5594a5b0c35ba64c7c1fcb7ad4536fd05cdd
XJL0930 <2928004597@qq.com>
2024-04-01 22:10:50 +0800

    添加订单管理界面
    添加订单管理界面

--- frontend/router/index.js
+++ frontend/router/index.js
@@ -22,7 +22,7 @@ const router = createRouter({
     routes: [
         {
             path: '/',
-            redirect: "/testpage",
+            redirect: "/indentmanagepage",
 
         },
         {
