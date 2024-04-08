26ea6e749b18b7d4f74570711eca30ac6f3a6e96
Andeviking <422563809@qq.com>
2024-04-08 23:43:39 +0800

    添加科大讯飞前端接口

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@nutui/touch-emulator'
 import router from '../router/index.js'; // 导入路由实例
-createApp(App).use(router).mount('#app')
-
+const app = createApp(App)
+app.use(router).mount('#app')
 
 window.addEventListener('unhandledrejection', event => {
     // 阻止默认的处理方式（即将错误信息输出到控制台）
@@ -12,3 +12,7 @@ window.addEventListener('unhandledrejection', event => {
     // 打印错误详情
     console.error('Unhandled promise rejection:', event.reason);
 });
+
+app.config.errorHandler = function(err) {
+    console.log("global", err);
+};
