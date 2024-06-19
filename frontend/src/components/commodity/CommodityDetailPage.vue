a00b39a36716457d3edb6a7c80edb47b163c35dc
Andeviking <422563809@qq.com>
2024-06-19 22:20:10 +0800

    修改价格曲线

--- frontend/src/components/commodity/CommodityDetailPage.vue
+++ frontend/src/components/commodity/CommodityDetailPage.vue
@@ -263,7 +263,7 @@ const convert = (response) => {
     option.series[0].data.push(nowCurve.com_pc_price);
   }
 
-  option.xAxis.data.push(nowCurve.com_expirationDate.split(" ")[0]);
+  option.xAxis.data.push(response.com_expirationDate.split(" ")[0]);
   option.series[0].data.push(0);
 
   list.value = [];
